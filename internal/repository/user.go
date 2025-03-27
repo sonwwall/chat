2145ddc794d378c1801f9ae489c3a6006598a868
sonwwall <2233like@gmail.com>
2025-03-28 01:18:30 +0800

    完善注册逻辑中

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"chat/internal/model"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) CreateUser(user *model.User) *gorm.DB {
+	return r.db.Create(user)
+}
